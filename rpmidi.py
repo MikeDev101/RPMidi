c93bb3e30bcdd0dc59430f196dee03b43cab3865
MikeDEV <mikierules109@gmail.com>
2022-04-02 12:51:54 -0400

    Version 1.1
    * Fix timing bugs
    * Code cleanliness enhancements

--- rpmidi.py
+++ rpmidi.py
@@ -58,7 +58,7 @@ class RPMidi:
     def _duty_cycle(self, percent):
         return round((percent/100)*65535)
 
-    def play_note(self, note, channel, duty, time=None):
+    def play_note(self, note, channel, duty):
         if channel == "a0":
             self.ch_a_0.freq(round(self._pitch(note)))
             self.ch_a_0.duty_u16(self._duty_cycle(duty))
@@ -71,9 +71,6 @@ class RPMidi:
         elif channel == "b1":
             self.ch_b_1.freq(round(self._pitch(note)))
             self.ch_b_1.duty_u16(self._duty_cycle(duty))
-        if not time == None:
-            utime.sleep(time)
-            self.stop_channel(channel)
 
     def stop_channel(self, channel):
         if channel == "a0":
@@ -91,60 +88,101 @@ class RPMidi:
         self.ch_b_0.duty_u16(0)
         self.ch_b_1.duty_u16(0)
     
+    def _opcodes(self):
+        return [0x90, 0x91, 0x92, 0x93, 0x80, 0x81, 0x82, 0x83, 0xf0, 0xe0]
+    
+    def _play_note_opcodes(self):
+        return [0x90, 0x91, 0x92, 0x93]
+
+    def _stop_note_opcodes(self):
+        return [0x80, 0x81, 0x82, 0x83]
+    
+    def _end_song_opcodes(self):
+        return [0xf0, 0xe0]
+    
     def play_song(self, music):
         if type(music) == list:
             done = False
             tmp = []
             index = 0
             isReading = False
+            opcode = 0x00
             
             self.stop_all() # Silence any existing music
+            utime.sleep(1)
             
             while not done:
-                if index >= len(music): # Index out-of-bound protection
+                if index >= len(music):
+                    print("out of range while reading opcode")
+                    done = True
                     break
                 else:
-                    if music[index] in [0x90, 0x91, 0x92, 0x93]: # Check for voice play command within 4 channels
+                    # Read opcode
+                    if music[index] in self._opcodes():
+                        opcode = music[index]
+                        
+                        # Scan for timing data
                         isReading = True
                         tmp = []
                         tmp_index = 1
-                        while isReading: # Read next 2 bytes for timing info
-                            if not music[index + tmp_index] in [0x90, 0x91, 0x92, 0x93, 0x80, 0x81, 0x82, 0x83]:
-                                tmp.append(music[index + tmp_index])
-                                tmp_index += 1
+                        
+                        # Read next bytes for timing info
+                        while isReading: 
+                            if (index + tmp_index) >= len(music):
+                                done = True
+                                break
                             else:
-                                isReading = False
+                                if not music[index + tmp_index] in self._opcodes():
+                                    tmp.append(music[index + tmp_index])
+                                    tmp_index += 1
+                                else:
+                                    isReading = False
                         
-                        if len(tmp) != 0: # Play note according to instruction
-                            if music[index] == 0x90:
-                                self.play_note(tmp[0], "a0", 50)
-                            elif music[index] == 0x91:
-                                self.play_note(tmp[0], "b0", 50)
-                            elif music[index] == 0x92:
-                                self.play_note(tmp[0], "a1", 50)
-                            elif music[index] == 0x93:
-                                self.play_note(tmp[0], "b1", 50)
-                            if len(tmp) != 1: # Delay if the instruction data contains delay info
-                                delay = ((tmp[1]*256)+(tmp[2]))
+                        # Execute instruction
+                        if opcode in self._play_note_opcodes():
+                            # Play voice and goto next instruction or play and wait for x milliseconds
+                            if len(tmp) > 0:
+                                if opcode == 0x90:
+                                    self.play_note(tmp[0], "a0", 50)
+                                elif opcode == 0x91:
+                                    self.play_note(tmp[0], "b0", 50)
+                                elif opcode == 0x92:
+                                    self.play_note(tmp[0], "a1", 50)
+                                elif opcode == 0x93:
+                                    self.play_note(tmp[0], "b1", 50)
+                                
+                                if len(tmp) == 3:
+                                    delay = ((tmp[1]*256)+(tmp[2]))
+                                    utime.sleep_ms(delay)
+                            else:
+                                print("expecting at least one entry in tmp, got nothing")
+                                done = True
+                                break
+                            index += 1
+                            
+                        elif opcode in self._stop_note_opcodes():
+                            # Mute voice or mute and wait for x milliseconds
+                            if opcode == 0x80:
+                                self.stop_channel("a0")
+                            elif opcode == 0x81:
+                                self.stop_channel("b0")
+                            elif opcode == 0x82:
+                                self.stop_channel("a1")
+                            elif opcode == 0x83:
+                                self.stop_channel("b1")
+                                
+                            if len(tmp) >= 2:
+                                delay = ((tmp[0]*256)+(tmp[1]))
                                 utime.sleep_ms(delay)
-                        
-                    elif music[index] in [0x80, 0x81, 0x82, 0x83]: # Check for voice mute command within 4 channels
-                        if music[index] == 0x80:
-                            self.stop_channel("a0")
-                        elif music[index] == 0x81:
-                            self.stop_channel("b0")
-                        elif music[index] == 0x82:
-                            self.stop_channel("a1")
-                        elif music[index] == 0x83:
-                            self.stop_channel("b1")
-                        
-                    elif music[index] in [0xf0, 0xe0]:
-                        if music[index] == 0xf0: # Song is over, stop playing.
-                            done = True
-                        else:
-                            index = -1 # Song is looping, go back to beginning of song.
-                        self.stop_all()
-                    index += 1
-        else:
-            print("Expecting list, got {0}".format(type(music)))
-    
+                            index += 1
+                            
+                        elif opcode in self._end_song_opcodes():
+                            # End or loop the song
+                            if opcode == 0xf0: # Song is over, stop playing.
+                                done = True
+                                break
+                            else:
+                                done = False
+                                index = 0 # Song is looping, go back to beginning of song.
+                    else:
+                        index += 1
